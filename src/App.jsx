59e9534f45bf19349f48319aa7fb9a245b9ce2ba
Elanur Bulbul <126582672+elanurbulbul@users.noreply.github.com>
2024-06-13 01:37:03 +0300

    FooterComponentAdded

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar/Navbar.jsx';
-
+import Footer from './components/Footer'
 function App() {
   
   return (
     <div className='App'>
       <Navbar/>
       <h1>sheelo</h1>
-      
+      <Footer/>
     </div>
   );
 }
