4f8435eaa6fe11621e4d1ddf12f26702e477ef03
Elanur Bulbul <126582672+elanurbulbul@users.noreply.github.com>
2024-06-13 01:07:22 +0300

    NavbarComponentFınıshed

---
+++ src/components/Navbar/NavItem.jsx
@@ -0,0 +1,19 @@
+// src/components/Navbar/navItems.js
+export const NAV_ITEMS = [
+    {
+        label: 'FILMS',
+        href: '#',
+    },
+    {
+        label: 'TV SHOWS',
+        href: '#',
+    },
+    {
+        label: 'WATCH LIST',
+        href: '#',
+    },
+    {
+        label: 'PEOPLE',
+        href: '#',
+    },
+];
