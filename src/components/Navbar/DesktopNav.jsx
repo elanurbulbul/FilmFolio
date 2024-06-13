8b959b43de4fc639d809625b12d18a57a01e39c9
Elanur Bülbül <elanrbulbul@gmail.com>
2024-06-14 01:59:43 +0300

    EditedNavAnddotenvFİleupdated

--- src/components/Navbar/DesktopNav.jsx
+++ src/components/Navbar/DesktopNav.jsx
@@ -20,7 +20,7 @@ const DesktopNav = () => {
     const popoverContentBgColor = useColorModeValue('white', 'gray.800');
 
     return (
-        <Stack direction={'row'} spacing={4}>
+        <Stack direction={'row'} spacing={4} alignSelf={'center'}>
             {NAV_ITEMS.map((navItem) => (
                 <Box key={navItem.label}>
                     <Popover trigger={'hover'} placement={'bottom-start'}>
