091983c68d3c45da0542b483ca6019a56139ea5b
Elanur Bulbul <126582672+elanurbulbul@users.noreply.github.com>
2024-06-27 19:13:18 +0300

    ApiTested

--- src/pages/Films/index.jsx
+++ src/pages/Films/index.jsx
@@ -1,8 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const Films = () => {
+  const [movieList, setMovieList] = useState([]);
+  const getMovie = () => {
+    fetch(
+      "https://api.themoviedb.org/3/discover/movie?api_key=caf0f1e593b52bdbc2ca284e307ccbc3"
+    )
+      .then((res) => res.json())
+      .then((json) => setMovieList(json.results))
+      .catch((error) => console.error("Error fetching movies:", error));
+  };
+
+  useEffect(() => {
+    getMovie();
+  }, []);
+
   return (
-   <div>sasas</div>
+    <div>
+      {movieList.map((movie) => (
+        <img
+          key={movie.id}
+           src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+          alt={movie.title}
+        />
+     
+      ))}
+      0
+    </div>
   );
 };
 export default Films;
