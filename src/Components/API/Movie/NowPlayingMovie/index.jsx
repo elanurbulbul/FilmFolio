753b22f612e6bce30c3f931c9be5a2810aa07026
Elanur Bulbul <126582672+elanurbulbul@users.noreply.github.com>
2024-07-03 19:20:51 +0300

    CreatedNowPlayıngMovieComponent

---
+++ src/Components/API/Movie/NowPlayingMovie/index.jsx
@@ -0,0 +1,29 @@
+import React, { useEffect, useState } from "react";
+import List from "../../../MovieList/list";
+import { Box } from "@chakra-ui/react";
+
+const NowPlayingMovie = () => {
+  const [movieList, setMovieList] = useState([]);
+
+  const getMovie = () => {
+    const apiUrl = `${import.meta.env.VITE_API_BASE_URL}/movie/now_playing?language=en-US&page=1&api_key=${import.meta.env.VITE_API_KEY}`;
+
+    fetch(apiUrl)
+    .then((res) => res.json())
+    .then((json) => setMovieList(json.results))
+    .catch((error) => console.error("Error fetching movies:", error));
+  };
+
+  useEffect(() => {
+    getMovie();
+  }, []);
+
+  return (
+    <Box p="4">
+      <h2>Now Playing Movies</h2>
+      <List movieList={movieList} />
+    </Box>
+  );
+};
+
+export default NowPlayingMovie;
--- src/Pages/Movies/index.jsx
+++ src/Pages/Movies/index.jsx
@@ -1,9 +1,17 @@
 import React from "react";
 import PopulerMovie from "../../Components/API/Movie/PopulerMovie";
+import { Heading } from "@chakra-ui/react";
+import NowPlayingMovie from "../../Components/API/Movie/NowPlayingMovie";
 
 const Movies = () => {
   return (
-   <PopulerMovie/>
+    <>
+    <Heading>Populer movie</Heading>
+      <PopulerMovie/>
+      <Heading>NowPlaying movie</Heading>
+      <NowPlayingMovie/>
+    </>
+   
   );
 };
 export default Movies;
