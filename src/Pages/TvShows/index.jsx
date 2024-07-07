e5ba1c238cc107bbcaae8cf45fcf23753a138174
Elanur Bülbül <elanrbulbul@gmail.com>
2024-07-07 19:27:58 +0300

    CreatedOnTheAirTvandAddThistoPage

--- src/Pages/TvShows/index.jsx
+++ src/Pages/TvShows/index.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import PopulerTvShow from "../../Components/API/TvShow/PopulerTv";
 import AiringTodayTv from "../../Components/API/TvShow/AiringTodayTv"
+import OnTheAirTv from "../../Components/API/TvShow/OnTheAirTv"
+
 const TvShows = () => {
   return (
    <>
    <PopulerTvShow/>
    <AiringTodayTv/>
+   <OnTheAirTv/>
+   
    </>
   );
 };
