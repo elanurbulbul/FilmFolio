6ca7856add257c5777e8199bc258bc7a70672d0f
Elanur Bülbül <elanrbulbul@gmail.com>
2024-07-07 19:45:31 +0300

    CreatedTopRatedTvComponents

--- src/Pages/TvShows/index.jsx
+++ src/Pages/TvShows/index.jsx
@@ -1,16 +1,22 @@
 import React from "react";
+import { Heading } from "@chakra-ui/react";
 import PopulerTvShow from "../../Components/API/TvShow/PopulerTv";
-import AiringTodayTv from "../../Components/API/TvShow/AiringTodayTv"
-import OnTheAirTv from "../../Components/API/TvShow/OnTheAirTv"
+import AiringTodayTv from "../../Components/API/TvShow/AiringTodayTv";
+import OnTheAirTv from "../../Components/API/TvShow/OnTheAirTv";
+import TopRatedTv from "../../Components/API/TvShow/TopRatedTv";
 
 const TvShows = () => {
   return (
-   <>
-   <PopulerTvShow/>
-   <AiringTodayTv/>
-   <OnTheAirTv/>
-   
-   </>
+    <>
+      <Heading>Populer</Heading>
+      <PopulerTvShow />
+      <Heading>Airing Today </Heading>
+      <AiringTodayTv />
+      <Heading> On The Air </Heading>
+      <OnTheAirTv />
+      <Heading>Top Rated </Heading>
+      <TopRatedTv />
+    </>
   );
 };
 export default TvShows;
