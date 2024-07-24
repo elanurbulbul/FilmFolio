550f1f23a5e2282a6f36fb0dea69ce5cd10e081f
Elanur Bülbül <elanrbulbul@gmail.com>
2024-07-24 18:23:14 +0300

    DeletedSearchPartonHomepage

--- src/Pages/Homepage/index.jsx
+++ src/Pages/Homepage/index.jsx
@@ -7,32 +7,10 @@ import MoviesTrending from "../../Components/API/Trending/MoviesTrending/MoviesT
 import TvTrending from "../../Components/API/Trending/TvTrending/TvShowsTrending";
 
 const Homepage = () => {
-  const [searchTerm, setSearchTerm] = useState("");
   const [isWeekly, setIsWeekly] = useState(false); // Yeni state
-  const navigate = useNavigate();
-  const toast = useToast();
+ 
 
-  const handleInputChange = (e) => {
-    setSearchTerm(e.target.value);
-  };
-
-  const handleSearch = () => {
-    if (searchTerm.trim()) {
-      navigate(`/search?query=${encodeURIComponent(searchTerm.trim())}`);
-    } else {
-      toast({
-        position: "top",
-        containerStyle: {
-          width: "800px",
-          maxWidth: "100%",
-        },
-        title: "Please enter a search term.",
-        status: "warning",
-        duration: 3000,
-        isClosable: true,
-      });
-    }
-  };
+ 
 
   const handleToggle = () => {
     setIsWeekly(!isWeekly);
@@ -40,17 +18,7 @@ const Homepage = () => {
 
   return (
     <>
-      <Box>
-        <Input
-          placeholder="Search..."
-          value={searchTerm}
-          onChange={handleInputChange}
-          mb="4"
-        />
-        <Button onClick={handleSearch} colorScheme="blue">
-          Search
-        </Button>
-      </Box>
+      
      <MoviesTrending/>
      <TvTrending/>
     </>
