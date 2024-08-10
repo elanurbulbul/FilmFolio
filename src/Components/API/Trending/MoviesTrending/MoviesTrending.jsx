253c10a66158a5c9e0e5bad6be9f2231f50cd998
Elanur Bülbül <elanrbulbul@gmail.com>
2024-08-11 01:17:33 +0300

    EditedCompaniesComponentandTrendingComponents

--- src/Components/API/Trending/MoviesTrending/MoviesTrending.jsx
+++ src/Components/API/Trending/MoviesTrending/MoviesTrending.jsx
@@ -19,14 +19,14 @@ const MoviesTrending = () => {
        
 
         <Stack direction="row" display={"flex"} alignItems={"center"} spacing={2}>
-          <Text>Daily</Text>
+          <Text>DAILY</Text>
           <Switch
           size={{base:"md", md:"lg"}}
             colorScheme="teal"
             isChecked={isWeekly}
             onChange={handleToggle}
           />
-          <Text >Weekly</Text>
+          <Text >WEEKLY</Text>
         </Stack>
       </Flex>
       <Text mx={3} mt={2}  textAlign="start" fontSize={{base:"10px", md:"18px"}} >You can see weekly or daily movies...</Text>
