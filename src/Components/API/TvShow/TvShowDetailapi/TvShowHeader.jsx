ce4f2e896eb0a8a8a5382942dede315ff427c677
Elanur Bülbül <elanrbulbul@gmail.com>
2024-08-10 15:57:45 +0300

    EditedTvShowDetailPageComponent

--- src/Components/API/TvShow/TvShowDetailapi/TvShowHeader.jsx
+++ src/Components/API/TvShow/TvShowDetailapi/TvShowHeader.jsx
@@ -1,25 +1,22 @@
 import React from "react";
 import { Heading, Text } from "@chakra-ui/react";
 
-const TVShowHeader = ({ name, overview, seasons, episodes, showDetails }) => (
-  <>
-    <Heading as="h1" size="xl" my={4} textAlign="start">
-      {name}
-    </Heading>
-    {showDetails && (
-      <>
-        <Text fontSize="lg" mb={4}>
-          {overview}
-        </Text>
-        <Text fontSize="lg" fontWeight="bold" mb={2}>
-          Seasons: {seasons}
-        </Text>
-        <Text fontSize="lg" fontWeight="bold">
-          Episodes: {episodes}
-        </Text>
-      </>
-    )}
-  </>
+export const TVShowName = ({ name }) => (
+  <Heading as="h1" size="xl" my={4} textAlign="start">
+    {name}
+  </Heading>
 );
 
-export default TVShowHeader;
+export const TVShowDetails = ({ overview, seasons, episodes }) => (
+  <>
+    <Text fontSize="lg" mb={4}>
+      {overview}
+    </Text>
+    <Text fontSize="lg" fontWeight="bold" mb={2}>
+      Seasons: {seasons}
+    </Text>
+    <Text fontSize="lg" fontWeight="bold">
+      Episodes: {episodes}
+    </Text>
+  </>
+);
--- src/Components/API/TvShow/TvShowDetailapi/TvShowPoster.jsx
+++ src/Components/API/TvShow/TvShowDetailapi/TvShowPoster.jsx
@@ -3,11 +3,12 @@ import { Image,Box } from '@chakra-ui/react';
 
 const Poster = ({ posterPath, name }) => (
   
-  <Box marginRight={2} >
+  <Box marginRight={2} alignSelf="center" >
   <Image
     width="100%"
     height="auto"
     maxWidth="288px"
+    maxHeight="385px"
     aspectRatio="3/4"
     src={`https://image.tmdb.org/t/p/w500${posterPath}`}
     alt={name}
