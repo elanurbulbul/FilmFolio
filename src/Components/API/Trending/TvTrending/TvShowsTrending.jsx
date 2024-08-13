f9667c7996e3308c6cafb407261531798791190a
Elanur Bülbül <elanrbulbul@gmail.com>
2024-08-13 21:03:54 +0300

    EditedHompage

--- src/Components/API/Trending/TvTrending/TvShowsTrending.jsx
+++ src/Components/API/Trending/TvTrending/TvShowsTrending.jsx
@@ -14,19 +14,19 @@ const TvTrending = () => {
     <Box my={4}>
        <Flex justifyContent="space-between" alignItems="center" mx={3}>
         <Stack direction="column" display="flex">
-        <Heading whiteSpace={"nowrap"} textAlign="start" fontSize={{base:"22px", md:"30px"}}>TOP 20 SHOWS</Heading>
+        <Heading whiteSpace={"nowrap"} textAlign="start" fontSize={{base:"25px", md:"30px"}}>TOP 20 SHOWS</Heading>
         </Stack>
        
 
         <Stack direction="row" display={"flex"} alignItems={"center"} spacing={2}>
-          <Text>DAILY</Text>
+          <Text fontSize={{base:"md", md:"lg"}}>DAILY</Text>
           <Switch
           size={{base:"md", md:"lg"}}
             colorScheme="teal"
             isChecked={isWeekly}
             onChange={handleToggle}
           />
-          <Text >WEEKLY</Text>
+          <Text fontSize={{base:"md", md:"lg"}} >WEEKLY</Text>
         </Stack>
       </Flex>
       <Text mx={3} mt={2}  textAlign="start" fontSize={{base:"10px", md:"18px"}} >You can see weekly or daily tv shows...</Text>
