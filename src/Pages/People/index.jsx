deb8ff864cd1c3b18a5107bafa7b2f106c6bd06b
Elanur Bülbül <elanrbulbul@gmail.com>
2024-08-19 02:37:29 +0300

    CreatedPersonSearchComponent

--- src/Pages/People/index.jsx
+++ src/Pages/People/index.jsx
@@ -1,11 +1,20 @@
 import React from "react";
 import PopularPeople from "../../Components/API/Person/PopularPeople"
-import { Container } from "@chakra-ui/react";
+import { Container, Box, Heading } from "@chakra-ui/react";
+import PersonSearch from "../../Components/API/Search/PersonSearch";
 
 const People = () => {
   return (
     <Container maxW={{ base: 'container.sm', md: "container.lg", lg: "container.lg", xl: "container.xl" }}>
-       <PopularPeople/>
+        <Box my={10} px={3}>
+            <Heading textAlign="start" mb={3}>
+                FAMOUS PEOPLE
+            </Heading>
+
+            <PersonSearch/>
+            <PopularPeople/>
+
+        </Box>
     </Container>
    
   );
