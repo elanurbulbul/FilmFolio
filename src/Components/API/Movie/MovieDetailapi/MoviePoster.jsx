16549a1e712735ca9cc96af75988cb62bc643d34
Elanur Bülbül <elanrbulbul@gmail.com>
2024-09-02 17:19:16 +0300

    FixedPosterError

--- src/Components/API/Movie/MovieDetailapi/MoviePoster.jsx
+++ src/Components/API/Movie/MovieDetailapi/MoviePoster.jsx
@@ -1,10 +1,15 @@
 import React from "react";
-import { Box, Image } from "@chakra-ui/react";
+import { Box, Image,Icon } from "@chakra-ui/react";
+import { FaPhotoFilm } from "react-icons/fa6";
 
 const MoviePoster = ({ posterPath, title }) => {
+  const placeholderThumbnail = 'https://via.placeholder.com/640x360.png?text=';
+
   return (
-    <Box marginRight={2} >
-      <Image
+    <Box marginRight={2} alignSelf="center" >
+      {posterPath ? (
+        <>
+           <Image
         width="100%"
         height="auto"
         maxWidth="240px"
@@ -13,6 +18,45 @@ const MoviePoster = ({ posterPath, title }) => {
         alt={title}
         borderRadius="lg"
       />
+         
+          
+        </>
+      ) : (
+        <Box
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+          borderColor="gray.700"
+          aspectRatio="2/3"
+          width="100%"
+          maxWidth="240px"
+          height="auto"
+          borderRadius="lg"
+          position="relative"
+
+        >
+          <Image
+            width="100%"
+            height="100%"
+            objectFit="cover"
+            borderRadius="lg"
+            src={placeholderThumbnail}
+            alt="No Poster Available"
+
+          />
+          <Box
+            position="absolute"
+            top="50%"
+            left="50%"
+            transform="translate(-50%, -50%)"
+            color="gray.500"
+            fontSize="50px"
+          >
+            <Icon as={FaPhotoFilm} />
+          </Box>
+        </Box>
+      )}
+     
     </Box>
   );
 };
--- src/Components/API/TvShow/TvShowDetailapi/TvShowPoster.jsx
+++ src/Components/API/TvShow/TvShowDetailapi/TvShowPoster.jsx
@@ -1,19 +1,64 @@
-import React from 'react';
-import { Image,Box } from '@chakra-ui/react';
+import React from "react";
+import { Box, Image,Icon } from "@chakra-ui/react";
+import { FaPhotoFilm } from "react-icons/fa6";
 
-const Poster = ({ posterPath, name }) => (
-  
-  <Box marginRight={2} alignSelf="center" >
-  <Image
-    width="100%"
-    height="auto"
-    maxWidth="240px"
-    aspectRatio="2/3"
-    src={`https://image.tmdb.org/t/p/w500${posterPath}`}
-    alt={name}
-    borderRadius="lg"
-  />
-</Box>
-);
+const TvShowPoster = ({ posterPath, name }) => {
+  const placeholderThumbnail = 'https://via.placeholder.com/640x360.png?text=';
 
-export default Poster;
+  return (
+    <Box marginRight={2} alignSelf="center" >
+      {posterPath ? (
+        <>
+           <Image
+        width="100%"
+        height="auto"
+        maxWidth="240px"
+        aspectRatio="2/3"
+        src={`https://image.tmdb.org/t/p/w500${posterPath}`}
+        alt={name}
+        borderRadius="lg"
+      />
+         
+          
+        </>
+      ) : (
+        <Box
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+          borderColor="gray.700"
+          aspectRatio="2/3"
+          width="100%"
+          maxWidth="240px"
+          height="auto"
+          borderRadius="lg"
+          position="relative"
+
+        >
+          <Image
+            width="100%"
+            height="100%"
+            objectFit="cover"
+            borderRadius="lg"
+            src={placeholderThumbnail}
+            alt="No Poster Available"
+
+          />
+          <Box
+            position="absolute"
+            top="50%"
+            left="50%"
+            transform="translate(-50%, -50%)"
+            color="gray.500"
+            fontSize="50px"
+          >
+            <Icon as={FaPhotoFilm} />
+          </Box>
+        </Box>
+      )}
+     
+    </Box>
+  );
+};
+
+export default TvShowPoster;
